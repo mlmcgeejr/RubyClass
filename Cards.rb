29f722045c57cbd00aa25e8cdc1190066317fc4e
mlmcgeejr <larry@mcgeemail.com>
2015-02-02 23:57:50 +0000

    Card Deal code

---
+++ Cards.rb
@@ -0,0 +1,14 @@
+def deal(card_array)
+  poker_hand = []
+  card_array = card_array.shuffle
+  poker_hand = card_array.first(5)
+end
+
+deck = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
+
+my_hand = deal(deck)
+puts "My first hand is " + my_hand.inspect
+puts " "
+ 
+my_hand = deal(deck)
+  puts "My second hand is " + my_hand.inspect
