47296bb638b42d2e0c71d858091088d7ced68ee5
mlmcgeejr <larry@mcgeemail.com>
2015-02-05 00:09:57 +0000

    Classwork for Class#4

---
+++ classy.rb
@@ -0,0 +1,33 @@
+class Person
+  attr_accessor :first_name, :last_name
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  def full_name
+    return @last_name + ", " + @first_name
+  end
+  
+  def say_something
+    puts "Hello Classy!  My name is " + @first_name
+  end
+end
+
+larry = Person.new("Larry", "McGee")
+
+puts larry.full_name + " says.... "
+people = []
+people.push Person.new("John", "Doe")
+people.push Person.new("Larry", "McGee")
+people.push Person.new("Les", "Porter")
+
+people[0].say_something
+people[1].say_something
+people[2].say_something
+
+puts " "
+people.each do |person|
+  person.say_something
+end
---
+++ hashfun.rb
@@ -0,0 +1,3 @@
+hash1 = {"Larry" => "A&E", "Barbara" => "Dr. Belton", "Alicia" => "Strathmore"}
+puts hash1.invert
+puts hash1
